06bdf382ad2019882c65abd3d67b4969022599d9
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-12-25 23:47:06 +0530

    all planets tab implemented

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -5,6 +5,32 @@ import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import IconButton from "@material-ui/core/IconButton";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import StarIcon from "@material-ui/icons/Star";
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridListMain: {
+    transform: "translateZ(0)",
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+  },
+});
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -53,7 +79,24 @@ class Home extends Component {
     this.setState({ value: val });
   };
 
+  planetClickHandler = (planetId) => {
+    let planetData = this.state.planetList;
+
+    let objIndex = planetData.findIndex((obj) => obj.id === planetId);
+
+    if (planetData[objIndex].isFavourite === true) {
+      planetData[objIndex].isFavourite = false;
+    } else {
+      planetData[objIndex].isFavourite = true;
+    }
+
+    console.log(planetData);
+
+    this.setState({ planetList: planetData });
+  };
+
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Header />
@@ -69,7 +112,44 @@ class Home extends Component {
           </Tabs>
         </AppBar>
         <TabPanel value={this.state.value} index={0}>
-          All Planets details
+          <div className="left">
+            <GridList
+              cellHeight={150}
+              cols={4}
+              className={classes.gridListMain}
+            >
+              {this.state.planetList.map((id) => (
+                <GridListTile
+                  key={"grid" + id.id}
+                  className="planets-grid-item"
+                >
+                  <img
+                    src="https://cdn.mos.cms.futurecdn.net/nGUneraFwt2iCnuu2iSWFf.jpg"
+                    alt={id.id}
+                  />
+                  <GridListTileBar
+                    title={id.name}
+                    classes={{
+                      root: classes.titleBar,
+                      title: classes.title,
+                    }}
+                    actionIcon={
+                      <IconButton
+                        aria-label={`info about ${id.id}`}
+                        onClick={() => this.planetClickHandler(id.id)}
+                      >
+                        {id.isFavourite === false ? (
+                          <StarBorderIcon className={classes.title} />
+                        ) : (
+                          <StarIcon color="primary" />
+                        )}
+                      </IconButton>
+                    }
+                  ></GridListTileBar>
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
         </TabPanel>
         <TabPanel value={this.state.value} index={1}>
           Fav Planets details
@@ -79,4 +159,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
